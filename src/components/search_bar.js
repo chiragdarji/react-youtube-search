eaff3e590e697d1f59dd7ff5fd89a57c32bfde83
Chirag Darji <chiragdarji.savli@gmail.com>
2018-08-12 21:44:15 +0530

    feature video list

---
+++ src/components/search_bar.js
@@ -0,0 +1,24 @@
+// Export, Class, State
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { term: 'Starting value' };
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term} 
+                    onChange={ event => this.setState({ term: event.target.value}) }  />
+
+                value of the input: {this.state.term}
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
